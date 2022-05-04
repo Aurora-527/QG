9c9a2c1d4e9ff5216cbf1cf6f4ab6e5168c4a263
AURORA427 <you@example.com>
2022-05-04 23:20:48 +0800

    none

--- little_group/week_2/LinearRegression.py
+++ little_group/week_2/LinearRegression.py
@@ -3,20 +3,23 @@ import pandas as pd
 #梯度下降
 class LinearRegression1:
     def __init__(self):
-        self.interception = None #截距
-        self.coef = None #即w，权重
-        self._theta = None
-
-    def fit(self, xtrain, ytrain):
-        x_b = np.hstack([np.ones((len(xtrain), 1)), xtrain])#添加样本特征数据
-        self._theta = np.linalg.inv(x_b.T.dot(x_b)).dot(x_b.T).dot(ytrain)#梯度下降公式
-        self.coef = self._theta[1:]
-        self.interception = self._theta[0]
-        return self
-
-    def predict(self, xtest):
-        x_b = np.hstack([np.ones((len(xtest), 1)), xtest])
-        return x_b.dot(self._theta)
+        self.alpha = 0.001 #步长
+        self.num = 1000 #迭代次数
+        self.w = None
+        
+ 
+    def fit(self,X,y):
+        X = np.hstack((X,np.ones(X.shape[0]).reshape(-1,1)))
+        N, m = X.shape
+        self.w = np.zeros(m)
+        for i in range(self.num):
+            grad = 2*X.T @ (X @ self.w - y)/N
+            self.w -= self.alpha * grad 
+        return self    
+ 
+    def predict(self,X):
+        x_b = np.hstack([np.ones((len(X),1)),X]) #第一个特征为1，故须创建一个矩阵与其拼接
+        return x_b.dot(self.w)
         
 #最小二乘
 class LinearRegression2:
@@ -31,8 +34,7 @@ class LinearRegression2:
         self.w_ = (X.T * X ).I * X.T * y
         return self
     def predict(self,X):
-        '''根据参数传递的样本X，delattr样本数据进行预测。
-        '''
+        '''根据参数传递的样本X数据进行预测。'''
         # 将X转换为矩阵
         X = np.asmatrix(X.copy())
         result = X * self.w_
